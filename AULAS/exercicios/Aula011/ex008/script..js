bd06398aa1ed67a7cbfcda2b35a71606776c24dc
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-04-08 19:23:04 -0300

    ex008

---
+++ AULAS/exercicios/Aula011/ex008/script..js
@@ -0,0 +1,17 @@
+/*
+var vel =30.5
+
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if(vel > 60){
+    console.log('MULTADO')
+} else{
+    console.log('Use cinto de segurança sempre')
+} */
+
+    var pais = 'EUA'
+    console.log(`Vivendo em ${pais}`)
+    if(pais == 'Brasil'){
+        console.log('Brasileiro!')
+    } else{
+        console.log('Estrangeiro')
+    }
---
+++ AULAS/exercicios/Aula011/ex008/velocidade.js
@@ -0,0 +1,8 @@
+var vel =30.5
+
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if(vel > 60){
+    console.log('MULTADO')
+} else{
+    console.log('Use cinto de segurança sempre')
+}
