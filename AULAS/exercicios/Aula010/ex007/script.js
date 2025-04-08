8362403fcc65843e81022d275ff5649251cacc9e
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-04-07 21:07:02 -0300

    Update script.js

--- AULAS/exercicios/Aula010/ex007/script.js
+++ AULAS/exercicios/Aula010/ex007/script.js
@@ -1,14 +1,16 @@
-onload = function(){
+onload = function() {
+    var btn = document.getElementById('btnSomar')
+    var res = document.getElementById('res')
 
-    function somar(){
+    btn.addEventListener('click', somar)
+
+    function somar() {
         var tn1 = document.getElementById('txtn1')
         var tn2 = document.getElementById('txtn2')
         var n1 = Number(tn1.value)
         var n2 = Number(tn2.value)
-        var soma
-        
         var soma = n1 + n2
 
-        res.innerText = `A soma entre ${soma}`
+        res.innerText = `A soma entre ${n1} e ${n2} é ${soma}`
     }
 }
