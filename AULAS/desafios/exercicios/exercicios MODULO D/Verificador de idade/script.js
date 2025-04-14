e8b002cedcdfa7260e5724ba14ef493751cb363b
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-04-14 15:52:51 -0300

    Criando o 2ex

---
+++ AULAS/desafios/exercicios/exercicios MODULO D/Verificador de idade/script.js
@@ -0,0 +1,7 @@
+function verificar(){
+    var nascimento = document.getElementById('inasc')
+    var sexo = document.getElementById('ismas')
+    var sexo = document.getElementById('isfem')
+}
+
+window.alert(`${sexo}`)
