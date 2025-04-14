859587071764dd96a6b67227cbb015d705654072
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-04-14 15:12:01 -0300

    terminando 1 ex

--- AULAS/desafios/exercicios/exercicios MODULO D/Interface de horario/script.js
+++ AULAS/desafios/exercicios/exercicios MODULO D/Interface de horario/script.js
@@ -5,4 +5,18 @@ function carregar(){
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora >= 0 && hora <12){
+        img.src = 'imagem-de-dia.jpg'
+        document.body.style.background='#e2cd9f'
+        //Bom dia!
+    } else if(hora >= 12 && hora <18){
+        //BOA TARDE
+        img.src = 'imagem-de-tarde.jpg'
+        document.body.style.background = '#b9846f'
+    }else{
+        //Boa noite
+        img.src ='imagem-de-noite.jpg'
+        document.body.style.background = '#515154'
+    }
 }
