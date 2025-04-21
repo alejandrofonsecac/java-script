92000bcb1a8fa6eccf1079d460362148b8817a2b
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-04-21 15:38:45 -0300

    Terminei

--- AULAS/desafios/exercicios/exercicios MODULO D/Verificador de idade/script.js
+++ AULAS/desafios/exercicios/exercicios MODULO D/Verificador de idade/script.js
@@ -1,25 +1,45 @@
 function verificar(){
-    var data = newDateI()
-    var ano = datagetFullYear()
-    var fano = document.getElementById('inasc')
-    var res= document.querySelector('div#res')
+    event.preventDefault()
 
-    if(fano.ariaValueMax.length == 0 || Number(fano.value) > ano){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('itxtano')
+    var res = document.querySelector('div#res')
+
+    if(fano.value.length == 0 || Number(fano.value) > ano){
         alert('[ERRO] Verifique os dados e tente novamente!')
-    } else{
+    } else {
         var fsex = document.getElementsByName('sexo')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
         if (fsex[0].checked){
             genero = 'Homem'
-        } else if(fsex[1].checked){
+            if (idade <= 10){
+                img.setAttribute('src', 'menino.jpg')
+            } else if (idade <= 18){
+                img.setAttribute('src', 'homem-adolescente.jpg')
+            } else if (idade <= 50){
+                img.setAttribute('src', 'homem-adulto.jpg')
+            } else {
+                img.setAttribute('src', 'homem-idoso.jpg')
+            }
+        } else if (fsex[1].checked){
             genero = 'Mulher'
+            if (idade <= 10){
+                img.setAttribute('src', 'menina.jpg')
+            } else if (idade <= 18){
+                img.setAttribute('src', 'mulher-adolescente.jpg')
+            } else if (idade <= 50){
+                img.setAttribute('src', 'mulher-adulta.jpg')
+            } else {
+                img.setAttribute('src', 'mulher-idosa.jpg')
+            }
         }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     } 
 }
-        
-    
-
-
-
