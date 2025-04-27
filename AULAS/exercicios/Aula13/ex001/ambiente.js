2d128c56ba724a10ff7d75c79de0994a01dd5ff7
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-04-27 08:55:16 -0300

    Create ambiente.js

---
+++ AULAS/exercicios/Aula13/ex001/ambiente.js
@@ -0,0 +1,16 @@
+ var i = 1
+ while (i < 6){
+    console.log('Tudo bem?')
+ }
+
+ console.log('Que bom!')
+ 
+ 
+ /*
+ console.log('Tudo bem?')
+ console.log('Tudo bem?')
+ console.log('Tudo bem?')
+ console.log('Tudo bem?')
+ console.log('Tudo bem?')
+ console.log('Tudo bem?')
+ */
