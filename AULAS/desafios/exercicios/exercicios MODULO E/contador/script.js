325c459df5a9a0edd95985837a23d1cffad54c39
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-04-29 19:12:59 -0300

    HTML complet
    Falta JavaScript

---
+++ AULAS/desafios/exercicios/exercicios MODULO E/contador/script.js
@@ -0,0 +1,15 @@
+function contar(){
+
+    var n1 = document.getElementById('ini')
+    var n2 = document.getElementById('ifim')
+    var cont = document.getElementById('icont')
+    var res = null
+
+    if(n1 == 0 || n2 == 0){
+        msg.innerHTML = 'Não é possivel realizar isso, verifique os valores acima'
+    }
+
+    for(cont; n1 =< n2; var res = 1){
+        msg.innerHTML = ``
+    }
+}
