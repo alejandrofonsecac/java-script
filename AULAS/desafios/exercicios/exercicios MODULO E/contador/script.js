7b950775f60b8888b85f51416d89857d6e3fbd39
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-04-29 20:44:56 -0300

    att

--- AULAS/desafios/exercicios/exercicios MODULO E/contador/script.js
+++ AULAS/desafios/exercicios/exercicios MODULO E/contador/script.js
@@ -1,15 +1,29 @@
-function contar(){
+function contar() {
+    let n1 = document.getElementById('ini')
+    let n2 = document.getElementById('ifim')
+    let passo = document.getElementById('icont')
+    let msg = document.getElementById('msg')
 
-    var n1 = document.getElementById('ini')
-    var n2 = document.getElementById('ifim')
-    var cont = document.getElementById('icont')
-    var res = null
+    let ini = Number(n1.value)
+    let fim = Number(n2.value)
+    let pas = Number(passo.value)
 
-    if(n1 == 0 || n2 == 0){
-        msg.innerHTML = 'Não é possivel realizar isso, verifique os valores acima'
+    if (ini === 0 || fim === 0 || pas === 0) {
+        msg.innerHTML = 'Não é possível realizar isso, verifique os valores acima.'
+        return
     }
 
-    for(cont; n1 =< n2; var res = 1){
-        msg.innerHTML = ``
+    msg.innerHTML = 'Contando:<br>'
+
+    if (ini < fim) {
+        for (let i = ini; i <= fim; i += pas) {
+            msg.innerHTML += `${i} 👉 `
+        }
+    } else {
+        for (let i = ini; i >= fim; i -= pas) {
+            msg.innerHTML += `${i} 👉 `
+        }
     }
+
+    msg.innerHTML += '🏁'
 }
